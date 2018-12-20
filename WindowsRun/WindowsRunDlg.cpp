1b3c6b6b95c5c2ff12b46a5ab3539eebce7953e0
云通 <yuntong@thtf.com.cn>
2018-12-20 16:00:28 +0800

    上传工程

---
+++ WindowsRun/WindowsRunDlg.cpp
@@ -0,0 +1,230 @@
+﻿
+// WindowsRunDlg.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "WindowsRun.h"
+#include "WindowsRunDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CWindowsRunDlg 对话框
+
+
+
+CWindowsRunDlg::CWindowsRunDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_WINDOWSRUN_DIALOG, pParent)
+	, m_strKey(_T(""))
+	, m_strValue(_T(""))
+	, m_strStatus(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CWindowsRunDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_KEY_NAME, m_strKey);
+	DDX_Text(pDX, IDC_EDIT_KEY_VALUE, m_strValue);
+	DDX_Text(pDX, IDC_STATIC_RESULT, m_strStatus);
+}
+
+BEGIN_MESSAGE_MAP(CWindowsRunDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_SELECT, &CWindowsRunDlg::OnBnClickedButtonSelect)
+	ON_BN_CLICKED(IDC_BUTTON_ON, &CWindowsRunDlg::OnBnClickedButtonOn)
+	ON_BN_CLICKED(IDC_BUTTON_OFF, &CWindowsRunDlg::OnBnClickedButtonOff)
+END_MESSAGE_MAP()
+
+
+// CWindowsRunDlg 消息处理程序
+
+BOOL CWindowsRunDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CWindowsRunDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CWindowsRunDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CWindowsRunDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CWindowsRunDlg::OnBnClickedButtonSelect()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CFileDialog dlg(TRUE);
+	if (IDOK == dlg.DoModal())
+	{
+		m_strKey = dlg.GetFileTitle();
+		m_strValue = dlg.GetPathName();
+		UpdateData(FALSE);
+	}
+}
+
+
+void CWindowsRunDlg::OnBnClickedButtonOn()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	UpdateData();
+	HKEY   hKey;
+
+	//找到系统的启动项   
+	LPCTSTR lpRun = _T("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+	CString strValue;
+	strValue.Format("\"%s\"", m_strValue);
+
+	//打开启动项Key   
+	long lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpRun, 0, KEY_WRITE, &hKey);
+	if (lRet == ERROR_SUCCESS)
+	{
+		//添加注册  
+		RegSetValueEx(hKey, m_strKey, 0, REG_SZ, (BYTE*)strValue.GetBuffer(), strValue.GetLength());
+		strValue.ReleaseBuffer();
+		RegCloseKey(hKey);
+		m_strStatus = "设置成功";
+	}
+	else
+	{
+		m_strStatus = "设置失败";
+	}
+	UpdateData(FALSE);
+}
+
+
+void CWindowsRunDlg::OnBnClickedButtonOff()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	UpdateData();
+	HKEY   hKey;
+	//找到系统的启动项   
+	LPCTSTR lpRun = _T("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+	//打开启动项Key   
+	long lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpRun, 0, KEY_WRITE, &hKey);
+	if (lRet == ERROR_SUCCESS)
+	{
+		//删除注册  
+		RegDeleteValue(hKey, m_strKey);
+		RegCloseKey(hKey);
+		m_strStatus = "取消成功";
+	}
+	else
+	{
+		m_strStatus = "取消失败";
+	}
+	UpdateData(FALSE);
+}
